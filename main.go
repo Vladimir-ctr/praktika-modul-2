77364e48e7560d3a5bd78e0290b26e74dc04d71c
Vladimir <morganspb4@gmail.com>
2026-02-27 16:11:33 +0300

    Вторая часть

--- main.go
+++ main.go
@@ -1,45 +1,106 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type User struct {
 	ID      string
 	Name    string
-	Balance float32
+	Balance float64
 }
 
-func (u *User) Deposit(amount float32) {
+func (u *User) Deposit(amount float64) {
 	u.Balance += amount
-	fmt.Printf("Ваш баланс пополнен на: %.2f, текущий остаток: %.2f\n", amount, u.Balance)
 }
 
-func (u *User) Withdrawn(amount float32) {
-	if u.Balance >= amount {
-		u.Balance -= amount
-		fmt.Printf("Списано %.2f, Ваш остаток %.2f\n", amount, u.Balance)
-	} else {
-		fmt.Println("Ошибка: недостаточно средств. Ваш баланс:\n", u.Balance)
+func (u *User) Withdrawn(amount float64) error {
+	if u.Balance < amount {
+		return errors.New("insufficient funds")
+	}
+	u.Balance -= amount
+	return nil
+}
+
+type Transaction struct {
+	FromID string
+	ToID   string
+	Amount float64
+}
+
+type PaymentSystem struct {
+	Users        map[string]*User
+	Transactions []Transaction
+}
+
+func (ps *PaymentSystem) AddUser(id, name string, balance float64) {
+	if ps.Users == nil {
+		ps.Users = make(map[string]*User)
+	}
+	ps.Users[id] = &User{
+		ID:      id,
+		Name:    name,
+		Balance: balance,
 	}
 }
 
+func (ps *PaymentSystem) AddTransaction(t Transaction) {
+	// Добавления транзакций в очередь (добавлие элемента в слайс)
+	ps.Transactions = append(ps.Transactions, t)
+}
+
+func (ps *PaymentSystem) ProcessingTransactions() error {
+	for _, t := range ps.Transactions {
+		sender, senderExists := ps.Users[t.FromID]
+		if !senderExists {
+			return fmt.Errorf("Sender not found")
+		}
+
+		receiver, recExists := ps.Users[t.ToID]
+		if !recExists {
+			return fmt.Errorf("Receiver not found")
+		}
+
+		err := sender.Withdrawn(t.Amount) //списание средств с пользователя
+		if err != nil {
+			return fmt.Errorf("insufficient funds for User: %s", t.FromID)
+		}
+
+		receiver.Deposit(t.Amount)
+		fmt.Printf("Balance User: %s changed on %.2f. Now balance %.2f\n", t.ToID, t.Amount, receiver.Balance)
+	}
+
+	ps.Transactions = nil
+	return nil
+}
+
 func main() {
-	user1 := &User{"1", "First", 150.35}
-	user2 := &User{"2", "Second", 1539.73}
-	user3 := &User{"3", "Third", 539.00}
+	paymentSystem := &PaymentSystem{
+		Users:        make(map[string]*User),
+		Transactions: make([]Transaction, 0),
+	}
 
-	user1.Deposit(132.35)
-	user1.Withdrawn(32.00)
-	fmt.Printf("%+v\n", user1)
-	fmt.Println()
+	fmt.Println("Создаю UserID: 1 с балансом 1000")
+	fmt.Println("Создаю UserID: 2 с балансом 500")
 
-	user2.Deposit(132.35)
-	user2.Withdrawn(32.00)
-	fmt.Printf("%+v\n", user2)
-	fmt.Println()
+	paymentSystem.AddUser("1", "Artem", 1000)
+	paymentSystem.AddUser("2", "Dmitry", 500)
 
-	user3.Deposit(132.35)
-	user3.Withdrawn(32.00)
-	fmt.Printf("%+v\n", user3)
-	fmt.Println()
+	fmt.Println("Перевожу с UserID: 1 на UserID: 2 сумму в размере 200")
+	fmt.Println("Перевожу с UserID: 2 на UserID: 1 сумму в размере 50")
+
+	paymentSystem.AddTransaction(Transaction{FromID: "1", ToID: "2", Amount: 200})
+	paymentSystem.AddTransaction(Transaction{FromID: "2", ToID: "1", Amount: 50})
+
+	paymentSystem.ProcessingTransactions()
+
+	for id, user := range paymentSystem.Users {
+		fmt.Printf("User %s (%s): balance = %.2f\n", id, user.Name, user.Balance)
+	}
+
+	fmt.Println("Итого")
+	fmt.Println("У первого пользователя должно получиться 850")
+	fmt.Println("У второго пользователя должно получиться 650")
 
 }
