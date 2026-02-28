ebfca2bd98b6830bbef7a5ed0ea69ba131f5e92a
Vladimir <morganspb4@gmail.com>
2026-02-28 23:01:57 +0300

    Третья часть

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"sync"
 )
 
 type User struct {
@@ -32,6 +33,7 @@ type Transaction struct {
 type PaymentSystem struct {
 	Users        map[string]*User
 	Transactions []Transaction
+	ch           chan Transaction
 }
 
 func (ps *PaymentSystem) AddUser(id, name string, balance float64) {
@@ -48,9 +50,10 @@ func (ps *PaymentSystem) AddUser(id, name string, balance float64) {
 func (ps *PaymentSystem) AddTransaction(t Transaction) {
 	// Добавления транзакций в очередь (добавлие элемента в слайс)
 	ps.Transactions = append(ps.Transactions, t)
+
 }
 
-func (ps *PaymentSystem) ProcessingTransactions() error {
+func (ps *PaymentSystem) ProcessingTransactions(tr Transaction) error {
 	for _, t := range ps.Transactions {
 		sender, senderExists := ps.Users[t.FromID]
 		if !senderExists {
@@ -64,15 +67,29 @@ func (ps *PaymentSystem) ProcessingTransactions() error {
 
 		err := sender.Withdrawn(t.Amount) //списание средств с пользователя
 		if err != nil {
-			return fmt.Errorf("insufficient funds for User: %s", t.FromID)
+			return fmt.Errorf("insufficient funds for User: %s\n", t.FromID)
 		}
 
 		receiver.Deposit(t.Amount)
+
 		fmt.Printf("Balance User: %s changed on %.2f. Now balance %.2f\n", t.ToID, t.Amount, receiver.Balance)
 	}
 
 	ps.Transactions = nil
 	return nil
+}
+
+func worker(ps *PaymentSystem, ch <-chan Transaction, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for tr := range ch {
+		err := ps.ProcessingTransactions(tr)
+		if err != nil {
+			fmt.Printf("Transaction failed: %v", err)
+		} else {
+			fmt.Println("Function comleted")
+		}
+	}
 }
 
 func main() {
@@ -93,14 +110,26 @@ func main() {
 	paymentSystem.AddTransaction(Transaction{FromID: "1", ToID: "2", Amount: 200})
 	paymentSystem.AddTransaction(Transaction{FromID: "2", ToID: "1", Amount: 50})
 
-	paymentSystem.ProcessingTransactions()
+	var wg sync.WaitGroup
+	ch := make(chan Transaction)
+
+	for i := 1; i <= 6; i++ {
+		wg.Add(1)
+		go worker(paymentSystem, ch, &wg)
+	}
+
+	for _, t := range paymentSystem.Transactions {
+		ch <- t
+	}
+	close(ch)
+	wg.Wait()
 
 	for id, user := range paymentSystem.Users {
 		fmt.Printf("User %s (%s): balance = %.2f\n", id, user.Name, user.Balance)
 	}
 
 	fmt.Println("Итого")
-	fmt.Println("У первого пользователя должно получиться 850")
-	fmt.Println("У второго пользователя должно получиться 650")
+	fmt.Printf("У первого пользователя должно получиться 850, а получилось %.2f\n", paymentSystem.Users["1"].Balance)
+	fmt.Printf("У второго пользователя должно получиться 650, а получилось %.2f", paymentSystem.Users["2"].Balance)
 
 }
